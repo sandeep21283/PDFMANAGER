c75645839765ec897e8aa1482d962a601a4eddfc
sandeep21283 <108021509+sandeep21283@users.noreply.github.com>
2025-04-14 19:17:28 +0000

    fifth commit

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -17,7 +17,7 @@ export default function DashboardLayout() {
   }
 
   if (!user) {
-    return <Navigate to="/login" state={{ from: location }} replace />;
+    return <Navigate to="/update-password" state={{ from: location }} replace />;
   }
 
   const handleLogout = async () => {

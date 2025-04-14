d381cf5501f60196bb726b65061e759fc182420b
sandeep21283 <108021509+sandeep21283@users.noreply.github.com>
2025-04-14 19:48:44 +0000

    fifth commit

--- src/App.tsx
+++ src/App.tsx
@@ -22,9 +22,10 @@ function App() {
           </Route>
           <Route element={<DashboardLayout />}>
             <Route path="/" element={<Dashboard />} />
-            <Route path="/update-password" element={<UpdatePassword />} />
+            
             <Route path="/pdf/:id" element={<PDFView />} />
           </Route>
+          <Route path="/update-password" element={<UpdatePassword />} />
           <Route path="/shared/:id" element={<SharedPDF />} />
         </Routes>
       </Router>
